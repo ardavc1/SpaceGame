71bb47a398213853718e0ae3a1cb000ff78e3d89
Arda Avcı <wonderluxct@gmail.com>
2023-09-10 20:30:56 +0300

    Update OyunEkrani.java

--- src/OyunEkrani.java
+++ src/OyunEkrani.java
@@ -18,7 +18,6 @@ public class OyunEkrani extends JFrame {
         ekran.setSize(800, 600);
         ekran.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
         Oyun oyun = new Oyun();
         oyun.requestFocus();
         oyun.addKeyListener(oyun);
